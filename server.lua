ec74e25041ba67cbb6d55d9d74fc440bac691c98
THC <88347509+THCVP@users.noreply.github.com>
2021-11-13 03:53:12 -0800

    Delete server.lua

--- server.lua
+++
@@ -1,7 +0,0 @@
-
-local resname = GetCurrentResourceName()
-print("\n--------------------------------------------------------------------------")
-print("\n"..resname.." ^1has started")
-print("\n^4Author: ^2Mosheba")
-print("\n^6Forked Resource By: THC")
-print("\n--------------------------------------------------------------------------")
